954bb24ab9860b924391d5f4931a8962e127ef0f
Vykash <banci.lorenzo@gmail.com>
2018-10-23 22:11:34 +0200

    Correzione Carousel Masonry

--- js/custom.js
+++ js/custom.js
@@ -14,32 +14,32 @@ jQuery(document).ready(function() {
         $('.navbar-toggler').toggleClass('cross-icon-close-toggle');
     });
     
-    jQuery('.masonry .carousel-image').each(function(){
+    jQuery('.masonry .masonry-image').each(function(){
         var x = jQuery(this).index();
         var y = jQuery(this).parent().index();
         jQuery(this).attr('data-order',x).attr('data-col',y);
     });
     
-    var colN = jQuery('.masonry .carousel-col').lenght;
+    var colN = jQuery('.masonry .masonry-col').lenght;
     jQuery('.masonry-filters li a').on('click', function(e){
         e.preventDefault();
         
         var target = jQuery(this).parent('li').attr('data-target');
         if(target != 'all'){
-            jQuery('.masonry .carousel-col .carousel-image').hide();
-            jQuery('.masonry .carousel-image[data-cat="'+target+'"]').each(function(i){
-                   jQuery(this).appendTo(jQuery('.masonry .carousel-col').eq(i));
+            jQuery('.masonry .masonry-col .masonry-image').hide();
+            jQuery('.masonry .masonry-image[data-cat="'+target+'"]').each(function(i){
+                   jQuery(this).appendTo(jQuery('.masonry .masonry-col').eq(i));
                    jQuery(this).show();
                 i++;
             });
         }else{
-            jQuery('.masonry .carousel-col .carousel-image').show();
-            jQuery('.masonry .carousel-image').each(function(c){
+            jQuery('.masonry .masonry-col .masonry-image').show();
+            jQuery('.masonry .masonry-image').each(function(c){
                 
                 
                 var i = jQuery(this).attr('data-order');
                 var i2 = jQuery(this).next().attr('data-order');
-                jQuery('.masonry .carousel-col[data-col="'+c+'"]');
+                jQuery('.masonry .masonry-col[data-col="'+c+'"]');
                 
                 
                 if(i2 == 0){
