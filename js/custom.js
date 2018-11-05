0e36b6a7b6607e260c11370c51a7fa92e54225fa
Vykash <banci.lorenzo@gmail.com>
2018-11-05 19:57:55 +0100

    Slider JS indice + slider JS con timer

--- js/custom.js
+++ js/custom.js
@@ -1,81 +1,183 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
 
-  /*$(document).scroll(function () {
-    var $nav = $('.navbar');
-    var $navlink = $('.nav-link')
-      $nav.toggleClass('dark-light',$(this).scrollTop() > $nav.height());
-  });*/
-   
-    $('.navbar-toggler').on('click',function(){
+    //NAVBAR TOGGLER
+    
+    $('.navbar-toggler').on('click', function () {
         $('.right-nav-box').toggleClass('margin-toggle');
     });
-    $('.navbar-toggler').on('click',function(){
+    $('.navbar-toggler').on('click', function () {
         $('.navbar-toggler').toggleClass('cross-icon-close-toggle');
     });
+    //////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+    
+    
+    
+    //MASONRY GALLERY
+    
     //assegno indici agli elementi
-    jQuery('.masonry-wrap .masonry-image').each(function(){
+    jQuery('.masonry-wrap .masonry-image').each(function () {
         var x = jQuery(this).index();
         var y = jQuery(this).parent().index();
-        jQuery(this).attr('data-order',x).attr('data-col',y);
-    }); 
-    jQuery('.masonry-filters li a').on('click', function(e){
+        jQuery(this).attr('data-order', x).attr('data-col', y);
+    });
+    jQuery('.masonry-filters li a').on('click', function (e) {
         e.preventDefault();
         var colN = jQuery('.masonry-wrap .masonry-col').length;
 
-        
+
         var target = jQuery(this).parent('li').attr('data-target');
-        if(target != 'all'){
+        if (target != 'all') {
             jQuery('.masonry-wrap .masonry-col .masonry-image').hide();
-            jQuery('.masonry-wrap .masonry-image[data-cat="'+target+'"]').each(function(i){
-                if(i >= colN){
+            jQuery('.masonry-wrap .masonry-image[data-cat="' + target + '"]').each(function (i) {
+                if (i >= colN) {
                     i = 0;
                 }
                 jQuery(this).appendTo(jQuery('.masonry-wrap .masonry-col').eq(i));
                 jQuery(this).show();
 
-                
+
             });
-        }else{
+        } else {
             jQuery('.masonry-wrap .masonry-col .masonry-image').show();
             c = 0;
             h = 0;
-            jQuery('.masonry-wrap .masonry-image').each(function(){
-                var g = jQuery('.masonry-wrap .masonry-image[data-col="'+c+'"]').length-1;
-                
-                jQuery('.masonry-wrap .masonry-image[data-col="'+c+'"][data-order="'+h+'"]').appendTo(jQuery('.masonry-wrap .masonry-col').eq(c));
-                
-                if(h == g){
+            jQuery('.masonry-wrap .masonry-image').each(function () {
+                var g = jQuery('.masonry-wrap .masonry-image[data-col="' + c + '"]').length - 1;
+
+                jQuery('.masonry-wrap .masonry-image[data-col="' + c + '"][data-order="' + h + '"]').appendTo(jQuery('.masonry-wrap .masonry-col').eq(c));
+
+                if (h == g) {
                     c++;
                     h = 0;
-                }else{
+                } else {
                     h++;
                 }
-            
+
             });
         }
     });
-    
-    ///////////////////////////////////////
-    
+    //////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+
     //TABS
     
-    jQuery('.menu-items li a').on('click',function(e){
+    jQuery('.menu-items li a').on('click', function (e) {
         e.preventDefault();
         // ottengo il data-target del link
         var attrLink = jQuery(this).attr('data-target')
         //rimuovo tutti gli elementi active
         jQuery('.who-we-are').removeClass('active');
         //aggiungo la classe active all' elemento con il 'data-target' uguale a quello del link
-        jQuery('.who-we-are[data-target='+attrLink+']').addClass('active')
-          
-    })
-    
+        jQuery('.who-we-are[data-target=' + attrLink + ']').addClass('active')
 
-    //////////////////////////////////////
-    //FOOTER SLIDER
+    });
+    //////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+
     
-    jQuery('.slider-button-right').on('click', function(){
+    //SLIDER 'on click' CON INDICE (PRIMA VERSIONE)
+    
+    jQuery('.brands-inner .brand-item').each(function () {
+        var index = jQuery(this).index()
+        jQuery(this).attr('data-order', index);
+    });
+    var x = jQuery('.brands-inner .brand-item').length
+    var i = 0;
+    var y = x;
+    jQuery('.slider-button-right').on('click', function () {
+        var currentImg = jQuery('.brand-item.slider-active');
+        i++
+        y = i
+        if (i == x) {
+            i = 0;
+        }
+        console.log(i + 'sono i')
+        jQuery('.brand-item[data-order=' + i + ']').addClass('slider-active');
+        currentImg.removeClass('slider-active');
+    });
+    jQuery('.slider-button-left').on('click', function () {
+        var currentImg = jQuery('.brand-item.slider-active');
+        y--
+        i = y
+        console.log(y + "sono y")
+        if (y == -1) {
+            y = x - 1;
+        }
+        jQuery('.brand-item[data-order=' + y + ']').addClass('slider-active');
+        currentImg.removeClass('slider-active');
+    });
+
+    //////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+
+
+    //SLIDER 'on click' CON INDICE (VERSIONE MIGLIORATA)
+
+    /*var x = 0; //contatore unico per le immagini
+    var y = jQuery('.brands-inner .brand-item').length;
+
+    jQuery('.brands-inner .brand-item').each(function(){
+            var index = jQuery(this).index();
+            jQuery(this).attr('data-order', index);
+        });
+    jQuery('.slider-button-right').on('click',function(){
+        var currentImg = jQuery('.brand-item.slider-active');
+        //proseguo verso destra incrementando 'x' ad ogni click
+        //se 'x' dovesse essere uguale al numero di elementi - 1 ( ovvero all'ultima immagine dello slider )
+        //sottraggo l' attuale valore di 'x' al valore di 'y'(Il numero di immagini)
+            if(x == y -1){
+                x = x - y;
+            }
+        x++;
+        jQuery('.brand-item[data-order='+x+']').addClass('slider-active');
+            currentImg.removeClass('slider-active'); 
+    });
+    jQuery('.slider-button-left').on('click',function(){
+        var currentImg = jQuery('.brand-item.slider-active');
+        //stesso procedimento ma decrementando il valore di 'x'(per tornare indietro)
+            if (x == 0){
+                x = y;
+            };
+        x--;
+        jQuery('.brand-item[data-order='+x+']').addClass('slider-active');
+            currentImg.removeClass('slider-active');
+    });*/
+
+    ///////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+    
+    //SLIDER CON TIMER
+    
+        var x = 0;
+        var y = jQuery('.brands-inner .brand-item').length;
+
+        jQuery('.brands-inner .brand-item').each(function () {
+            var index = jQuery(this).index();
+            jQuery(this).attr('data-order', index);
+        });
+    
+    //FUNZIONE CUSTOM
+    function autoSlider() {
+        var currentImg = jQuery('.brand-item.slider-active');
+            if(x == y -1){
+                x = x - y;
+            }
+        x++;
+        jQuery('.brand-item[data-order='+x+']').addClass('slider-active');
+        currentImg.removeClass('slider-active');
+    };
+    //Richiamo la funzione
+    
+    setInterval(autoSlider,2000);
+    
+    //////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+    
+    //SLIDER 'on click' CON '.next' '.prev'
+
+    /*jQuery('.slider-button-right').on('click', function(){
         //assegno l'immagine con la classe active ad una variabile
     var currentImg = jQuery('.brand-item.slider-active');
         //controllo se l'ultima immagine è quella attiva, se attiva ricomincio dalla prima immagine
@@ -99,6 +201,6 @@ jQuery(document).ready(function() {
                jQuery('.slider-active').prev().addClass('slider-active');
                jQuery(currentImg).removeClass('slider-active')
            }; 
-    }); 
-    
+    }); */
+
 });
